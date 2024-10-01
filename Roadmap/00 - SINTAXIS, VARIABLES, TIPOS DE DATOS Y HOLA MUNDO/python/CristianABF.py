8662bd132aeb685f6b642e09e7df1057cb59142c
CristianABF <cristianabf2002@gmail.com>
2024-10-01 20:01:11 -0300

    Create CristianABF.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CristianABF.py
@@ -0,0 +1,28 @@
+# www.phyton.org
+
+# esto es un comentario en una linea
+
+"""
+esto es un comentario
+multilinea
+se abre con tres comillas dobles
+"""
+
+'''
+aunque tambien
+puede abrirse con 
+comillas simples
+'''
+
+# variables y constantes
+my_variable = "Mi variable"
+MY_CONSTANTE = "Esto es una constante por convención" # convención
+
+# datos primitivos
+my_int = 5
+my_float = 5.1
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+
+my_print( "¡Hola Phyton!")
